7fc92ca938ac73add04742e54099d77d35489a9b
Jacy Wang <jacywj@gmail.com>
2014-11-12 20:43:03 -0500

    Finish first version

---
+++ paper_rock_scissors.rb
@@ -0,0 +1,87 @@
+
+module Reference
+  REFERENCE = { "P" => "Paper", "R" => "Rock", "S" => "Scissor" }
+end
+
+class Player
+
+  include Reference
+
+  def initialize
+    @player_choice
+  end
+
+  def choice
+    begin 
+      puts "Choose one: (#{REFERENCE.keys.join("/")})"
+      @player_choice = gets.chomp.upcase
+    end until REFERENCE.keys.include?(@player_choice)
+    @player_choice
+  end 
+end
+
+class Computer
+
+  include Reference
+
+  def initialize
+    @computer_choice
+  end
+
+  def choice
+    @computer_choice = REFERENCE.keys.sample
+  end
+
+end
+
+class PaperRockScissors
+
+  include Reference
+
+  def initialize
+    welcome
+    @player = Player.new
+    @computer = Computer.new
+  end
+
+  def welcome
+    puts "Welcome to play Paper Rock Scissors!"
+  end
+
+  def run
+    player_choice = @player.choice
+    computer_choice = @computer.choice
+    display_choice(player_choice, computer_choice)
+    check_winner(player_choice, computer_choice)
+    play_again
+  end
+
+  def display_choice(player_choice, computer_choice)
+    puts "You chose #{REFERENCE[player_choice]}"
+    puts "Computer chose #{REFERENCE[computer_choice]}"
+  end
+
+  def check_winner(player_choice, computer_choice)
+    if player_choice == computer_choice
+      puts "It's a tie!"
+    elsif (player_choice == "R" && computer_choice == "S") || 
+          (player_choice == "P" && computer_choice == "R") ||
+          (player_choice == "S" && computer_choice == "P")
+      puts "You won!"
+    else
+      puts "Computer won!"
+    end
+  end
+
+  def play_again
+    begin
+      puts "Play again? Y/N"
+      user_chocie = gets.chomp.upcase
+    end until ["Y", "N"].include?(user_chocie)
+    self.run if user_chocie == "Y"
+    exit if user_chocie == "N"
+  end
+
+end
+
+PaperRockScissors.new.run
